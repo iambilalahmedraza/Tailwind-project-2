464dc3ff51219be5ebccea69e36686f4e48c0048
BILAL AHMED RAZA <iambilalahmedraza@gmail.com>
2024-10-16 11:14:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,382 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="font-body bg-[#FEF4DE] h-full bg-[url('assets/bg.png')] w-full ">
+        <header className="">
+          <div className="">
+            <img src="src/assets/brown1.png" alt="" className="w-full " />
+            <div className="container flex  w-[77%] h-full   gap-20  mx-auto">
+              <div className="mt-20">
+                <h1 className="text-[70px] w-full text-[#663128] font-black">
+                  What the
+                </h1>
+                <h2 className="text-[#663128] w-full -mt-10 text-[45px] font-bold">
+                  B e a r &nbsp; B e a r s
+                </h2>
+                <p className="text-[#663128] mt-8 text-[16px] font-semibold">
+                  Lorem Ipsum is simply dummy text of the printing and <br />
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  <br />
+                  standard dummy text ever since the
+                </p>
+                <div className="flex gap-10 my-8">
+                  <button className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#fdd86a] duration-300 bg-[#F3CE5E] text-[#663128] w-[210px] h-[68px] rounded-full font-bold">
+                    Help me
+                  </button>
+                  <button className="text-[#663128] font-semibold">
+                    About Us
+                  </button>
+                </div>
+              </div>
+              <div className="">
+                <img src="src/assets/bears.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <div className="bg-[url('assets/brown2.png')] w-full bg-cover h-full">
+          <h1 className="text-white font-bold text-[60px]  pt-72 pb-10 flex justify-center items-center">
+            Services
+          </h1>
+        </div>
+        <div className="bg-[url('assets/brown3.png')] w-full flex flex-col justify-center items-center ">
+          <div className="container  w-[70%] flex flex-col gap-6  mx-auto">
+            <div className="flex gap-6 ">
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl h-[210px]">
+                <div className="">
+                  <img src="src/assets/i1.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i2.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i3.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i4.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-6 ">
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i5.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i6.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i2.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+              <div className="flex justify-center items-center shadow-lg gap-2 px-1 w-full text-[#7C4032]    bg-white rounded-2xl  h-[210px]">
+                <div className="">
+                  <img src="src/assets/i5.png" alt="" />
+                </div>
+                <div className="">
+                  <h1 className="text-[18px] font-extrabold">Round Rock</h1>
+                  <h2 className="text-[16px] -mt-2 font-semibold">
+                    P l u m b i n g
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="w-[210px] h-[66px] font-bold text-[#663128] rounded-full bg-[#F3CE5E] my-16 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#fdd86a] duration-300  ">
+            Help me
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full h-full testymonials">
+          <h1 className="text-[64px] py-8 shadow-lg text-[#7C4032] font-black">
+            Testymonials
+          </h1>
+          <div className="flex flex-col items-center gap-4 mx-10 mb-20">
+            <div className="flex gap-4">
+              <div className="w-full h-full bg-white border-2 rounded-2xl flex flex-col justify-center px-6 border-[#7C4032]">
+                <div className="flex flex-col gap-3 px-10 py-4">
+                  <h1 className="text-[#7C4032]  text-[16px] font-black ">
+                    Kathryn Murphy
+                  </h1>
+                  <p className="text-[14px]  text-[#333333]">
+                    Lorem Ipsum is simply dummy text of the printing and <br />{" "}
+                    typesetting industry. Lorem Ipsum has been the industry's{" "}
+                    <br /> standard dummy text ever since the 1500s, when an{" "}
+                    <br /> unknown printer took a galley of type and scrambled
+                  </p>
+                  <div className="flex gap-2">
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full h-full bg-white border-2 rounded-2xl flex flex-col justify-center px-6 border-[#7C4032]">
+                <div className="flex flex-col gap-3 px-10 py-4">
+                  <h1 className="text-[#7C4032]  text-[16px] font-black ">
+                    Kathryn Murphy
+                  </h1>
+                  <p className="text-[14px]  text-[#333333]">
+                    Lorem Ipsum is simply dummy text of the printing and <br />{" "}
+                    typesetting industry. Lorem Ipsum has been the industry's{" "}
+                    <br /> standard dummy text ever since the 1500s, when an{" "}
+                    <br /> unknown printer took a galley of type and scrambled
+                  </p>
+                  <div className="flex gap-2">
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <div className="w-full h-full bg-white border-2 rounded-2xl flex flex-col justify-center px-6 border-[#7C4032]">
+                <div className="flex flex-col gap-3 px-10 py-4">
+                  <h1 className="text-[#7C4032]  text-[16px] font-black ">
+                    Kathryn Murphy
+                  </h1>
+                  <p className="text-[14px]  text-[#333333]">
+                    Lorem Ipsum is simply dummy text of the printing and <br />{" "}
+                    typesetting industry. Lorem Ipsum has been the industry's{" "}
+                    <br /> standard dummy text ever since the 1500s, when an{" "}
+                    <br /> unknown printer took a galley of type and scrambled
+                  </p>
+                  <div className="flex gap-2">
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full h-full bg-white border-2 rounded-2xl flex flex-col justify-center px-6 border-[#7C4032]">
+                <div className="flex flex-col gap-3 px-10 py-4">
+                  <h1 className="text-[#7C4032]  text-[16px] font-black ">
+                    Kathryn Murphy
+                  </h1>
+                  <p className="text-[14px]  text-[#333333]">
+                    Lorem Ipsum is simply dummy text of the printing and <br />{" "}
+                    typesetting industry. Lorem Ipsum has been the industry's{" "}
+                    <br /> standard dummy text ever since the 1500s, when an{" "}
+                    <br /> unknown printer took a galley of type and scrambled
+                  </p>
+                  <div className="flex gap-2">
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                    <img
+                      src="src/assets/star.png"
+                      height={18}
+                      width={18}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="w-[210px] h-[66px] font-bold text-[#663128] rounded-full bg-[#F3CE5E] my-16 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#fdd86a] duration-300    mt-12 ">
+              Help me
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-full bg-white">
+          <h1 className="text-center text-[64px] py-12 text-[#7C4032] font-black ">
+            Contact Us
+          </h1>
+          <div className="flex items-center justify-center mb-16">
+            <div className="flex gap-16">
+              <div className="flex flex-col items-center gap-3">
+                <input
+                  type="text"
+                  className="bg-[#E5AF99] w-[374px] h-[56px] rounded-full"
+                />
+                <input
+                  type="text"
+                  className="bg-[#E5AF99] w-[374px] h-[56px] rounded-full"
+                />
+                <input
+                  type="text"
+                  className="bg-[#E5AF99] w-[374px] h-[56px] rounded-full"
+                />
+                <button className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 w-[169px] h-[56px] rounded-full border-2 border-[#7C4032]"></button>
+              </div>
+              <div className="flex flex-col gap-4 mt-10 text-[#333333] font-bold">
+                <div className="flex gap-5">
+                  <img src="src/assets/mail.png" alt="" className="w-6" />
+                  test@gmail.com
+                </div>
+                <div className="flex gap-5">
+                  <img src="src/assets/call.png" alt="" />
+                  +92 111 1111 111
+                </div>
+                <div className="flex gap-5 ml-1">
+                  <img src="src/assets/map.png" alt="" />
+                  2715 Ash Dr. San Jose, South Dakota 83475
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full bg-[#55352D] h-[122px]"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
